033f087ca31e959e2b9188886bc79aaef81dc532
Eric Andersen <ea33sports@me.com>
2018-10-15 16:40:27 -0600

    Half of inspiration layout... 10/15/18 4:40pm

---
+++ BuckIt2/InspirationTab/ModelControllers/InspirationController.swift
@@ -0,0 +1,14 @@
+//
+//  InspirationController.swift
+//  BuckIt2
+//
+//  Created by Eric Andersen on 10/15/18.
+//  Copyright © 2018 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+
+class InspirationController {
+    
+    
+}
