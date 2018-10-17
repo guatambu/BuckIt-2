5a5256c5323b9b49b7a0678a6aa12665b4c199fe
michael guatambu davis <guatambu@dundak.com>
2018-10-17 11:30:26 -0700

    adds MockData UserBucketLists

---
+++ BuckIt2/New Group/MockData Structs/MockDataUserBucketLists.swift
@@ -0,0 +1,27 @@
+//
+//  MockDataUserBucketLists.swift
+//  BuckIt2
+//
+//  Created by Kelly Johnson on 10/17/18.
+//  Copyright © 2018 DunDak, LLC. All rights reserved.
+//
+
+import UIKit
+
+struct MockDataUserBucketLists {
+    
+    var rodrigoList: [BucketListItem] = [MockDataBucketListItems.item1, MockDataBucketListItems.item5, MockDataBucketListItems.item10, MockDataBucketListItems.item15]
+    
+    var sangitaList: [BucketListItem] = [MockDataBucketListItems.item2, MockDataBucketListItems.item6, MockDataBucketListItems.item11, MockDataBucketListItems.item16]
+    
+    var dylonList: [BucketListItem] = [MockDataBucketListItems.item3, MockDataBucketListItems.item7, MockDataBucketListItems.item12, MockDataBucketListItems.item17]
+    
+    var dylonList: [BucketListItem] = [MockDataBucketListItems.item4, MockDataBucketListItems.item8, MockDataBucketListItems.item13, MockDataBucketListItems.item18]
+    
+    var parkList: [BucketListItem] = [MockDataBucketListItems.item19, MockDataBucketListItems.item25, MockDataBucketListItems.item21, MockDataBucketListItems.item23]
+    
+    var maggieList: [BucketListItem] = [MockDataBucketListItems.item20, MockDataBucketListItems.item24, MockDataBucketListItems.item22, MockDataBucketListItems.item24]
+}
+
+
+
